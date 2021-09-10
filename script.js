6989758832e84e58e262bfd43ef0062e5cddb5fb
DevrajPanwar <65825249+DevrajPanwar@users.noreply.github.com>
2021-09-10 23:00:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breatheAnimation();
+
+function breatheAnimation() {
+    text.innerHTML = "Breathe In";
+    container.className = 'container grow';
+
+    setTimeout(() =>{
+        text.innerHTML = "Hold On";
+
+        setTimeout(() =>{
+            text.innerHTML = "Breathe Out";
+            container.className = 'container shrink';
+
+        }, holdTime);
+
+    }, breatheTime);
+}
+
+setInterval(breatheAnimation, totalTime);
